35bfad84be0e29353cc0ce415b8256ac49ddb9cc
Mo Morsi <mmorsi@redhat.com>
2011-11-06 05:00:53 -0500

    encyclopedia parser improvements
      - pull content from new wiki
    
      - improve performance / compression

--- util/parser/parser.rb
+++ util/parser/parser.rb
@@ -21,17 +21,11 @@ require 'curb'
 ############################################################
 
 # constants to retrieve / parse nethack snapshot
-MW_URL      = "http://nethack.wikia.com/wiki/"
-MW_NS       = "http://www.mediawiki.org/xml/export-0.4/"
-MW_SNAPSHOT = "http://wikistats.wikia.com/n/ne/nethack/pages_current.xml.gz"
+MW_URL      = "http://nethackwiki.com"
 
 # articles to ignore in the app
 IGNORE_FILENAME_PATTERNS  =
-  [/^Talk:.*/, /^Template.*/, /.*\/Archive.*/,
-   /^User:.*/, /^User talk:.*/, /^User blog:.*/,
-   /^Blog:.*/, /^File:.*/, /^File talk:.*/, /^Forum:.*/,
-   /^Source:.*/, /^Source talk:.*/, /^.*\.c/, /^.*\.h/,
-   /^Wikihack:.*/, /^Wikihack talk:.*/ ]
+  [/^.*\.c/, /^.*\.h/, /^NetHack [0-9]\.[0-9].*/, /^All pages.*$/ ]
 
 # paginate encyclopedia to conform to android restrictions
 MAX_FILE_SIZE = 500000
@@ -44,10 +38,6 @@ WORKING_DIR = File.expand_path(File.dirname(__FILE__))
 
 # actual location to store the encyclopedia
 DST_DIR = "encyclopedia/"
-
-# location which to store the wiki snapshot
-WIKI_SNAPSHOT    = "snapshot.xml"
-WIKI_SNAPSHOT_GZ = "snapshot.xml.gz"
 
 # location which to make encyclopedia available so that
 # Android can find it
@@ -72,39 +62,39 @@ Dir.mkdir DST_DIR unless File.exists?(DST_DIR)
 File.symlink WORKING_DIR + "/" + DST_DIR, ASSETS_DIR unless File.exists?(ASSETS_DIR)
 Dir[DST_DIR + "/*"].each { |f| FileUtils.rm(f) }
 
-# retrieve the snapshot
-unless File.exist?(WIKI_SNAPSHOT)
-  printf "#{COLORS[:green]} Retrieving Snapshot..." ; STDOUT.flush
-  File.open(WIKI_SNAPSHOT_GZ, "w") { |f|
-    f.write Curl::Easy.http_get(MW_SNAPSHOT).body_str
+printf "#{COLORS[:green]} Retrieving pages list..." ; STDOUT.flush
+
+# retrieve Special::AllPages, pull the sectioned pages out of that
+all_pages_pages = []
+all_pages = Curl::Easy.http_get(MW_URL + "/wiki/Special:AllPages").body_str
+n = Nokogiri::HTML(all_pages)
+n.xpath("//td[@class='mw-allpages-alphaindexline']").each{ |first|
+  second = first.next_sibling.next_sibling
+  all_pages_pages << "from=#{first.children[0].inner_text.gsub(/\s/, '_')}&to=#{second.children[0].inner_text.gsub(/\s/, '_')}"
+}
+
+# retrieve the list of pages out of the section pages
+pages = []
+all_pages_pages.each { |app|
+  all_pages = Curl::Easy.http_get(MW_URL + "/mediawiki/index.php?title=Special:AllPages&#{app}").body_str
+  n = Nokogiri.HTML(all_pages)
+  n = n.xpath("//table[@class='mw-allpages-table-chunk']")[0]
+  n.xpath("//td/a").each { |a|
+    page = a.inner_text
+    if IGNORE_FILENAME_PATTERNS.select { |ifp| page =~ ifp }.empty?
+      pages << page
+    end
   }
-  `gunzip #{WIKI_SNAPSHOT_GZ}`
-  printf "#{COLORS[:red]} Done\n"
-end
+}
 
 i, registry_count, pos = 0, 0, 0
-pages = []
-
-# pull article titles out of the snapshot
-printf "#{COLORS[:blue]} Parsing snapshot..." ; STDOUT.flush
-doc = Nokogiri::XML(File.open(WIKI_SNAPSHOT))
-doc.xpath("//mw:mediawiki/mw:page", 'mw' => MW_NS).each { |page|
-    title = page.xpath('mw:title',  'mw' => MW_NS).text
-    if IGNORE_FILENAME_PATTERNS.select { |ifp| title =~ ifp }.empty?
-      # insertion sort
-      j = pages.index { |p| p > title }
-      j = 0 if j.nil?
-      pages.insert j, title
-    end
-}
-printf "#{COLORS[:red]} Done\n"
 
 # retrieve all articles
 #   (previously we were converting the wikitext in the snapshot,
 #    but no wikitext -> html converter renders the output as well as mediawiki itself)
 puts "#{COLORS[:green]} Retrieving wiki articles..."
 registry_file, output_file =  File.open("#{DST_DIR}/registry", "w"), nil
-pages.each { |title,data|
+pages.each { |title|
   unless File.exist?("#{DST_DIR}/#{registry_count}") && (File.size("#{DST_DIR}/#{registry_count}") < MAX_FILE_SIZE)
     puts "#{COLORS[:blue]}  Closing previous catalog file ##{registry_count}" unless registry_count == 0
     puts "#{COLORS[:blue]}  Opening new catalog file ##{registry_count += 1}"
@@ -113,9 +103,19 @@ pages.each { |title,data|
   end
 
   puts "  #{COLORS[:green]}(#{Time.now.strftime('%I:%M:%S%p')}) Retrieving article (#{i+=1}/#{pages.size}): '#{title}'" ; STDOUT.flush
-  text = Curl::Easy.http_get(MW_URL + title.gsub(/\s/, '_')).body_str
-  text = Nokogiri::HTML(text).xpath("//div[@id='WikiaArticle']").inner_html
+  text = Curl::Easy.http_get(MW_URL + '/wiki/' + title.gsub(/\s/, '_')).body_str
+  parser = Nokogiri::HTML(text).xpath("//div[@id='bodyContent']")
+  remove_content = []
+  remove_content << parser.search("//div[@id='siteSub']").first
+  remove_content << parser.search("//div[@id='contentSub']").first
+  remove_content << parser.search("//div[@id='jump-to-nav']").first
+  remove_content << parser.search("//div[@class='printfooter']").first
+  remove_content << parser.search("//div[@class='visualClear']").first
+  parser.search("//script").each { |s| remove_content << s }
+  remove_content.each { |r| r.remove unless r.nil? }
+  text = parser.inner_html
   text.gsub!(/\s+/, ' ')
+  text.gsub!(/<!--\s*\/*[a-zA-Z0-9]+\s*-->/, '')
   #text.gsub!(/\<img[^\>]*\>/, '') # FIXME need to pull in images
   output_file.write   text
   registry_file.write "#{title}#{REGISTRY_DELIM}" +

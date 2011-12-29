11c2b4d4b092fa2fb7e584e6570e9ac7613afd2d
Mo Morsi <mo@morsi.org>
2011-12-29 13:16:32 -0600

    back button now returns to previous encyclopedia page

--- src/org/morsi/android/nethack/redux/EncyclopediaPage.java
+++ src/org/morsi/android/nethack/redux/EncyclopediaPage.java
@@ -5,22 +5,26 @@ package org.morsi.android.nethack.redux;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.TextView;
-
 import org.morsi.android.nethack.redux.R;
 import org.morsi.android.nethack.redux.util.EncyclopediaEntry;
 
 // Show encyclopedia page screen when encyclopedia topic is clicked
 public class EncyclopediaPage extends Activity
 {
+
+
 	public void onCreate(Bundle savedInstanceState)
 	{
+
 		super.onCreate(savedInstanceState);
 		String current_page = getIntent().getExtras().getString("page");
+
 		setContentView(R.layout.encyclopedia_popup);
 		Button close_button = (Button) findViewById(R.id.encyclopedia_page_close);
 		EncyclopediaEntry entry = EncyclopediaActivity.encyclopedia.get(current_page);
@@ -39,12 +43,7 @@ public class EncyclopediaPage extends Activity
 
 				if(url.substring(0, 22).equals("fake://morsi.org/wiki/")){
 					String next_page = url.substring(22, url.length());
-
-					Intent EncPage = new Intent(EncyclopediaPage.this, EncyclopediaPage.class);
-					EncPage.putExtra("page", next_page);
-					startActivity(EncPage);
-					setResult(RESULT_OK);
-					finish();
+					NextActivity(next_page);
 					return true;
 				}
 				return false;
@@ -62,5 +61,69 @@ public class EncyclopediaPage extends Activity
 				finish();
 			}
 		});
+	}
+
+
+
+	//create previous page
+	public void NextActivity() {
+		int CurrentDepth = Integer.parseInt(getIntent().getExtras().getString("depth"));
+
+		if(CurrentDepth > 0){
+			Intent EncPage = new Intent(EncyclopediaPage.this, EncyclopediaPage.class);
+			String PrevPage = getIntent().getExtras().getString("0");
+			EncPage.putExtra("page", PrevPage );
+
+			//
+			for (int i=1; i<CurrentDepth; i++){
+				String aPage = getIntent().getExtras().getString(Integer.toString(i));
+				EncPage.putExtra(Integer.toString(i-1),aPage);
+			}
+
+			CurrentDepth--;
+			EncPage.putExtra("depth",Integer.toString(CurrentDepth));
+
+			startActivity(EncPage);
+			setResult(RESULT_OK);
+			finish();
+		}else{	//close the window if there aren't any more pages to go to
+			setResult(RESULT_OK);
+			finish();
+		}
+	}
+
+	//create clicked page
+	public void NextActivity(String NextPage) {
+		int CurrentDepth = Integer.parseInt(getIntent().getExtras().getString("depth"));
+		String current_page = getIntent().getExtras().getString("page");
+
+		Intent EncPage = new Intent(EncyclopediaPage.this, EncyclopediaPage.class);
+		EncPage.putExtra("page", NextPage);		//set the page for the next window
+
+		// import the page history from previous intent
+		for (int i=0; i<=CurrentDepth; i++){
+			String aPage = getIntent().getExtras().getString(Integer.toString(i));
+			EncPage.putExtra(Integer.toString(i+1), aPage);
+		}
+
+		// push current page onto the top of the history stack
+		EncPage.putExtra("0", current_page);
+		CurrentDepth++;
+		EncPage.putExtra("depth",Integer.toString(CurrentDepth));
+
+		startActivity(EncPage);
+		setResult(RESULT_OK);
+		finish();
+
+	}
+
+	// make back button return to previous encyclopedia page
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+			NextActivity();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
 	}
 }
